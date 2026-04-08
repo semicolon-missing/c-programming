5d3faeba161c6e2fb92ef8cd957d2093329a5889
Vivek Kumar Pandey <vkpandey150906@gmail.com>
2026-04-08 19:43:05 +0530

    merge_sort

--- 2_Sorting/merge_sort.c
+++ 2_Sorting/merge_sort.c
@@ -1,10 +1,81 @@
 #include <stdio.h>
 
+void merge(int arr[], int start, int end, int mid){
+    int i, j, k;
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
 
+    int arr1[n1], arr2[n2];
 
+    for (i = 0; i < n1; i++){
+        arr1[i] = arr[start + i];
+    }
+
+    for (j = 0; j < n2; j++){
+        arr2[j] = arr[mid + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = start;
+
+    while (i < n1 && j < n2){
+        if (arr1[i] < arr2[j])
+        {
+            arr[k] = arr1[i];
+            i++;
+        }
+        else{
+            arr[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = arr1[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = arr2[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(int arr[], int start, int end){
+    if (start < end)
+    {
+        int mid = (start + end - 1) / 2;
+        mergesort(arr, start, mid);
+        mergesort(arr, mid + 1, end);
+
+        merge(arr, start, end, mid);
+    }
+}
 
 
 int main() {
+    int arr[] = {12, 8, 3, 17, 9, 6};
+    int size = sizeof(arr) / sizeof(int);
     
+    printf("Unsorted array: {");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("}\n");
+
+    mergesort(arr, 0, size - 1);
+    printf("Sorted array: {");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("}\n");
+
+
     return 0;
 }
--- 2_Sorting/quick_sort.c
+++ 2_Sorting/quick_sort.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
 
-
-void quicksort(){
-
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int partition(int arr[], int p, int q){
-    int x = arr[p];
-    int i = 0;
-    int j;
-    for (int j = p + 1; j < q; j++) {
+int partition(int *arr, int start, int end){
+    int pivot = arr[end];
+    int index = start - 1;
+    for (int j = start; j < end; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            index++;
+            swap(&arr[index], &arr[j]);
+        }
         
+    }
+    swap(&arr[index + 1], &arr[end]);
+    return index + 1;
+}
+
+void quicksort(int *arr, int start, int end){
+    if (start < end){
+        int pi = partition(arr, start, end);
+        quicksort(arr, start, pi - 1);
+        quicksort(arr, pi + 1, end);
     }
 }
 
 int main() {
+    int arr[] = {12, 8, 3, 17, 9, 6};
+    int size = sizeof(arr) / sizeof(int);
     
+    printf("Unsorted array: {");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("}\n");
+    quicksort(arr, 0, 5);
+    printf("Sorted array: {");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("}\n");
+
     return 0;
 }
